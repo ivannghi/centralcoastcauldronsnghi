65753c6ea13756cefc3d134575edcc8442bf5a96
Ivan Nghi <inghi@calpoly.edu>
2023-10-28 16:59:56 -0700

    updated to use sqlalchemy funcs

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
 from sqlalchemy import create_engine
+import sqlalchemy
 
 def database_connection_url():
     dotenv.load_dotenv()
@@ -8,3 +9,8 @@ def database_connection_url():
     return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+
+metadata_obj = sqlalchemy.MetaData()
+cart_items = sqlalchemy.Table("cart_items", metadata_obj, autoload_with=engine)
+potions = sqlalchemy.Table("potions", metadata_obj, autoload_with=engine)
+carts = sqlalchemy.Table("carts", metadata_obj, autoload_with=engine)
